cfd3c0770302b1d5665b7f6750da6022d15be5c8
MuffyPuff <sjjeuseen@gmail.com>
2018-02-01 10:17:29 +0100

    Update global.h

--- src/global.h
+++ src/global.h
@@ -4,6 +4,13 @@
 #include "qtranslator.h"
 //#include <cmath>
 #include <QtMath>
+
+
+#define ALL(c) (c).begin(),(c).end()
+#define IN(x, c) (find(c.begin(), c.end(), x) != (c).end())
+#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+
 namespace
 {
 
