148780f745514ff7ed5256e69a89abd8c38f7def
Muf <rok.strah@tuta.io>
2018-02-26 18:58:09 +0100

    Name etc

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,14 @@ int
 main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	QCoreApplication::setApplicationName("StrahCalc");
+	QCoreApplication::setOrganizationName("rstrah");
+//	QCoreApplication::setOrganizationDomain("strah.ddns.net");
+
+
 	MainWindow w;
+	w.setWindowTitle("StrahCalc");
 	w.show();
 
 	return a.exec();
