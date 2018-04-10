60aef5f3089dd4a182f7e60e634b302ba4148d86
Muf <rok.strah@tuta.io>
2018-04-10 21:42:02 +0200

    unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ main(int argc, char* argv[])
 	QCoreApplication::setOrganizationName("rstrah");
 	QCoreApplication::setOrganizationDomain("rokstrah.com");
 
-//	MufExprParser mPar;
+	MufExprParser mPar;
 //	qDebug() << mPar.exprParseSY("2-3");
 //	qDebug() << mPar.exprParseSY("-[-x -x]");
 //	qDebug() << mPar.exprParseSY("2^x");
@@ -30,6 +30,25 @@ main(int argc, char* argv[])
 //	qDebug() << mPar.exprParseTD("x^(2+7*x)-(-7*x)");
 //	qDebug() << mPar.exprParseTD("x^(2+7*x)-(-x/-7)");
 //	qDebug() << mPar.exprParseTD("");
+
+	Q_ASSERT(mPar.exprParseTD("x") == "{x}");
+	Q_ASSERT(mPar.exprParseTD("0+x") == "{x}");
+	Q_ASSERT(mPar.exprParseTD("x+0") == "{x}");
+	Q_ASSERT(mPar.exprParseTD("0-x") == "{-\\left({x}\\right)}");
+	Q_ASSERT(mPar.exprParseTD("x-0") == "{x}");
+	Q_ASSERT(mPar.exprParseTD("0*x") == "{0}");
+	Q_ASSERT(mPar.exprParseTD("x*0") == "{0}");
+	Q_ASSERT(mPar.exprParseTD("1*x") == "{x}");
+	Q_ASSERT(mPar.exprParseTD("x*1") == "{x}");
+	Q_ASSERT(mPar.exprParseTD("0/x") == "{0}");
+	Q_ASSERT(mPar.exprParseTD("x/0") == "{\\infty}");
+	Q_ASSERT(mPar.exprParseTD("0^x") == "{0}");
+	Q_ASSERT(mPar.exprParseTD("1^x") == "{1}");
+	Q_ASSERT(mPar.exprParseTD("x^0") == "{1}");
+	Q_ASSERT(mPar.exprParseTD("x^1") == "{x}");
+	Q_ASSERT(mPar.exprParseTD("-x/-y") == "\\frac{{x}}{{y}}");
+	Q_ASSERT(mPar.exprParseTD("-x*-y") == "{{x}\\cdot{y}}");
+
 
 	MainWindow w;
 	w.setWindowTitle("StrahCalc");
