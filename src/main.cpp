ad73d1334ca20eae683d4284fd476d6e2d95fe6c
Muf <rok.strah@tuta.io>
2018-04-11 14:40:50 +0200

    edited unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -31,26 +31,26 @@ main(int argc, char* argv[])
 //	qDebug() << mPar.exprParseTD("x^(2+7*x)-(-x/-7)");
 //	qDebug() << mPar.exprParseTD("");
 
-	Q_ASSERT(mPar.exprParseTD("x") == "{x}");
-	Q_ASSERT(mPar.exprParseTD("0+x") == "{x}");
-	Q_ASSERT(mPar.exprParseTD("x+0") == "{x}");
-	Q_ASSERT(mPar.exprParseTD("0-x") == "{-\\left({x}\\right)}");
-	Q_ASSERT(mPar.exprParseTD("x-0") == "{x}");
-	Q_ASSERT(mPar.exprParseTD("0*x") == "{0}");
-	Q_ASSERT(mPar.exprParseTD("x*0") == "{0}");
-	Q_ASSERT(mPar.exprParseTD("1*x") == "{x}");
-	Q_ASSERT(mPar.exprParseTD("x*1") == "{x}");
-	Q_ASSERT(mPar.exprParseTD("0/x") == "{0}");
-	Q_ASSERT(mPar.exprParseTD("x/0") == "{\\infty}");
-	Q_ASSERT(mPar.exprParseTD("0^x") == "{0}");
-	Q_ASSERT(mPar.exprParseTD("1^x") == "{1}");
-	Q_ASSERT(mPar.exprParseTD("x^0") == "{1}");
-	Q_ASSERT(mPar.exprParseTD("x^1") == "{x}");
-	Q_ASSERT(mPar.exprParseTD("-x/-y") == "{\\frac{{x}}{{y}}}");
-	Q_ASSERT(mPar.exprParseTD("-x*-y") == "{{x}\\cdot{y}}");
-	Q_ASSERT(mPar.exprParseTD("x^(2+7*x)-(-x/-7)") ==
+	Q_ASSERT(mPar("x") == "{x}");
+	Q_ASSERT(mPar("0+x") == "{x}");
+	Q_ASSERT(mPar("x+0") == "{x}");
+	Q_ASSERT(mPar("0-x") == "{-\\left({x}\\right)}");
+	Q_ASSERT(mPar("x-0") == "{x}");
+	Q_ASSERT(mPar("0*x") == "{0}");
+	Q_ASSERT(mPar("x*0") == "{0}");
+	Q_ASSERT(mPar("1*x") == "{x}");
+	Q_ASSERT(mPar("x*1") == "{x}");
+	Q_ASSERT(mPar("0/x") == "{0}");
+	Q_ASSERT(mPar("x/0") == "{\\infty}");
+	Q_ASSERT(mPar("0^x") == "{0}");
+	Q_ASSERT(mPar("1^x") == "{1}");
+	Q_ASSERT(mPar("x^0") == "{1}");
+	Q_ASSERT(mPar("x^1") == "{x}");
+	Q_ASSERT(mPar("-x/-y") == "{\\frac{{x}}{{y}}}");
+	Q_ASSERT(mPar("-x*-y") == "{{x}\\cdot{y}}");
+	Q_ASSERT(mPar("x^(2+7*x)-(-x/-7)") ==
 	         "{{{x}^{{2}+{{7}\\cdot{x}}}}-{\\frac{{x}}{{7}}}}");
-	Q_ASSERT(mPar.exprParseTD("t-x/y+z") == "{{{t}-{\\frac{{x}}{{y}}}}+{z}}");
+	Q_ASSERT(mPar("t-x/y+z") == "{{{t}-{\\frac{{x}}{{y}}}}+{z}}");
 
 
 	MainWindow w;
